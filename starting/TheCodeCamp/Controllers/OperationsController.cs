835e914a4db4d251be84a9f3d092229b5ba81a5b
LINUXHOST\nilesh <nileshaj@live.com>
2019-07-02 06:30:49 -0400

    Introduced Functional API

---
+++ starting/TheCodeCamp/Controllers/OperationsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TheCodeCamp.Controllers
+{
+    public class OperationsController : ApiController
+    {
+        [HttpOptions]
+        [Route("api/refreshconfig")]
+        public IHttpActionResult RefreshAppSettings()
+        {
+            try
+            {
+                ConfigurationManager.RefreshSection("AppSettings");
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                return InternalServerError(ex);
+            }
+        }
+
+    }
+}
